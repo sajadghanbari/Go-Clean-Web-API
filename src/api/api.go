371ca39545864423fe022f31fc292a57a96499bc
Sajjad Ghanbari <a.s.ghanbari.j@gmail.com>
2025-09-09 19:26:06 +0330

    Read environment config from yml files added

--- src/api/api.go
+++ src/api/api.go
@@ -2,17 +2,19 @@ package api
 
 import (
 	"clean-web-api/api/routers"
-	
+	"clean-web-api/config"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 )
 
 func InitServer() {
+	cfg := config.GetConfig()
 	r := gin.New()
 	r.Use(gin.Logger(),gin.Recovery())
 
 	v1 := r.Group("/api/v1/")
 	health := v1.Group("/health")
 	routers.Health(health)
-	r.Run(":5050")
+	r.Run(fmt.Sprintf(":%s" ,cfg.Server.Port))
 }
